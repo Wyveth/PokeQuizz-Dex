a3c029b9cedd698a22bc7730b66e4a6335230e1b
Garrigue Benjamin <93991961+Wyveth@users.noreply.github.com>
2023-05-10 00:02:49 +0200

    AuthGuard with navigator.language OK

--- src/app/shared/guards/auth-guard.service.ts
+++ src/app/shared/guards/auth-guard.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-constructor() { }
+  constructor(private router:Router) { }
 
+  canActivate(): boolean|UrlTree {
+    let location = "EN";
+    switch (navigator.language.split('-')[0].toLocaleUpperCase())
+    {
+      case "FR" || "EN" || "ES" || "IT" || "DE" || "RU":
+        location = navigator.language.split('-')[0].toLocaleUpperCase();
+        break;
+      case "ZH":
+        location = "CN";
+        break;
+      case "KO":
+        location = "CO";
+        break;
+      case "JA":
+        location = "JP";
+        break;
+      default:
+        location = "EN";
+    }
+
+    console.log("AuthGuardService", location);
+
+    this.router.navigate([location, "pokedex"]);
+    return false;
+  }
 }
--- src/app/shared/modules/app-routing.module.ts
+++ src/app/shared/modules/app-routing.module.ts
@@ -3,15 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from 'src/app/views/home/home.component';
 import { PokedexComponent } from 'src/app/views/pokedex/pokedex.component';
 import { PokemonDetailsComponent } from 'src/app/views/pokemon-details/pokemon-details.component';
+import { AuthGuardService } from '../guards/auth-guard.service';
 
 const routes: Routes = [
-  { path: ':loc/home', component: HomeComponent },
+  { path: ':loc/home', component: HomeComponent, canActivate : [AuthGuardService] },
   { path: ':loc/pokedex', component: PokedexComponent },
   { path: ':loc/pokedex/pokemon/:id', component: PokemonDetailsComponent },
-  { path: '', component: HomeComponent },
-  { path: 'pokedex', redirectTo: 'EN/pokedex' },
-  { path: 'pokedex/pokemon/:id', redirectTo: 'pokedex/pokemon/:id' },
-  { path: 'home', redirectTo: 'EN/pokedex' },
+  { path: '', component: HomeComponent, canActivate : [AuthGuardService] },
   { path: '**', redirectTo: 'home' },
 ];
 
